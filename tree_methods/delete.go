4fcbf4260aa363a938b65bedce319a20131605d1
Edward <93082954+EDIKODESU@users.noreply.github.com>
2024-04-03 12:44:37 +0300

    Add files via upload

---
+++ tree_methods/delete.go
@@ -0,0 +1,42 @@
+package tree_methods
+
+import "fmt"
+
+func (imt *IncrementalMerkleTree) DeleteData(id int) error {
+	if imt.Root == nil {
+		fmt.Println("root is empty")
+		return fmt.Errorf("data is empty")
+	}
+
+	targetNode := imt.findCurrNodeByID(id)
+	if targetNode == nil {
+		fmt.Printf("block with id %d not exists\n", id)
+		return fmt.Errorf("block with id %d not exists\n", id)
+	}
+
+	if imt.Root == targetNode {
+		imt.Root = nil
+		fmt.Println("Root is delete")
+		return nil
+	}
+
+	deleteNode(targetNode)
+	return nil
+}
+
+// Функція для видалення вузла та оновлення шляху до кореня
+func deleteNode(node *Node) {
+	parent := node.Parent
+	if parent == nil {
+		node.Block = nil
+		return
+	}
+
+	if parent.Left == node {
+		parent.Left = nil
+	} else if parent.Right == node {
+		parent.Right = nil
+	}
+
+	updatePath(parent)
+}
---
+++ tree_methods/utils.go
@@ -0,0 +1,73 @@
+package tree_methods
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+// Оновлення шляху від доданого вузла до кореня
+func updatePath(node *Node) {
+	for node.Parent != nil {
+		parent := node.Parent
+
+		leftHash := ""
+		if parent.Left != nil {
+			leftHash = parent.Left.Block.Hash
+		}
+
+		rightHash := ""
+		if parent.Right != nil {
+			rightHash = parent.Right.Block.Hash
+		}
+
+		if leftHash != "" && rightHash != "" {
+			parent.Block.Hash = hashFunction(leftHash, rightHash)
+		}
+
+		node = parent
+	}
+}
+
+// Хеш-функція для обчислення нового хеша з двох вхідних хешів
+func hashFunction(leftHash, rightHash string) string {
+	data := []byte(leftHash + rightHash)
+	hash := sha256.Sum256(data)
+	return hex.EncodeToString(hash[:])
+}
+
+func (imt *IncrementalMerkleTree) findCurrNode(predicate func(*Node) bool) *Node {
+	if imt.Root == nil {
+		return nil
+	}
+
+	queue := []*Node{imt.Root}
+	for len(queue) > 0 {
+		currNode := queue[0]
+		queue = queue[1:]
+
+		if currNode.Left != nil {
+			queue = append(queue, currNode.Left)
+		}
+		if currNode.Right != nil {
+			queue = append(queue, currNode.Right)
+		}
+
+		if predicate(currNode) {
+			return currNode
+		}
+	}
+
+	return nil
+}
+
+func (imt *IncrementalMerkleTree) findCurrNodeByID(id int) *Node {
+	return imt.findCurrNode(func(node *Node) bool {
+		return node.Block != nil && node.Block.Id == id
+	})
+}
+
+func (imt *IncrementalMerkleTree) findCurrNodeByHash(hashData string) *Node {
+	return imt.findCurrNode(func(node *Node) bool {
+		return node.Block != nil && node.Block.Hash == hashData
+	})
+}
